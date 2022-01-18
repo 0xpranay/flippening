d97c58df89bfe2ac6d8865ebadb3ba29551de173
Pranay Reddy <pranayreddy1024@gmail.com>
2022-01-18 21:00:28 +0530

    Fully working product, with confetti on top

--- src/components/ProceedButton.tsx
+++ src/components/ProceedButton.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { Flex, Button } from "@chakra-ui/react";
-
+import JSConfetti from "js-confetti";
 import RenJS from "@renproject/ren";
 import { Bitcoin, Ethereum } from "@renproject/chains";
 
@@ -24,7 +24,7 @@ interface state {
 
 export default function ProceedButton() {
   const state = useSelector((state: state) => state);
-  const contractAddress = "0xd5bED1f0d9b4d4D473d49D414332F32dFd55FcBb";
+  const contractAddress = "0xf2b3B57EcF761f9e1e46f67402331f1EA1D06F12";
   const renJS = new RenJS("testnet", { useV2TransactionFormat: true });
 
   useEffect(() => {
@@ -34,89 +34,113 @@ export default function ProceedButton() {
     console.log(state);
   }
 
-  async function flippening() {
-    console.log("Generating deposit address...");
-    const amount = state.btcAmount.amount;
-    const choice = state.targetChoice.choice;
-    const address = state.ethAddress.address;
-    const provider = state.provider;
-    const signer = state.signer;
-    console.log("Choice is " + choice);
-    let contractFn = "";
-    switch (choice) {
-      case "WBTC":
-        contractFn = "receiveWrappedBTC";
-        break;
-      case "ETH":
-        contractFn = "receiveETH";
-        break;
-      case "AAVE":
-        contractFn = "stakeOnAave";
-        break;
-    }
-    const mint = await renJS.lockAndMint({
-      asset: "BTC",
-      from: Bitcoin(),
-      to: Ethereum(provider).Contract({
-        sendTo: contractAddress,
-        contractFn: contractFn,
-        contractParams: [
-          {
-            name: "_msg",
-            type: "bytes",
-            value: Buffer.from(`Depositing ${amount} BTC`),
-          },
-          {
-            name: "recipient",
-            type: "address",
-            value: address,
-          },
-        ],
-      }),
+  function throwConfetti() {
+    const jsconfetti = new JSConfetti();
+    jsconfetti.addConfetti({
+      emojis: ["🌈", "🦄"],
+      emojiSize: 80,
     });
+  }
+  async function flippening() {
+    try {
+      console.log("Generating deposit address...");
+      const amount = state.btcAmount.amount;
+      const choice = state.targetChoice.choice;
+      const address = state.ethAddress.address;
+      const provider = state.provider;
+      const signer = state.signer;
+      console.log("Choice is " + choice);
+      let contractFn = "";
+      switch (choice) {
+        case "WBTC":
+          contractFn = "receiveWrappedBTC";
+          break;
+        case "ETH":
+          contractFn = "receiveETH";
+          break;
+        case "AAVE":
+          contractFn = "stakeOnAave";
+          break;
+      }
 
-    // Modify below code
-    alert(`Deposit ${amount} BTC to ${mint.gatewayAddress}`);
-    mint.on("deposit", async (deposit) => {
-      // Details of the deposit are available from `deposit.depositDetails`.
+      console.log(
+        "Calling function " +
+          contractFn +
+          " at address " +
+          contractAddress +
+          " with user as " +
+          address
+      );
+      const mint = await renJS.lockAndMint({
+        asset: "BTC",
+        from: Bitcoin(),
+        to: Ethereum(provider).Contract({
+          sendTo: contractAddress,
+          contractFn: contractFn,
+          contractParams: [
+            {
+              name: "_msg",
+              type: "bytes",
+              value: Buffer.from(`Depositing ${amount} BTC`),
+            },
+            {
+              name: "recipient",
+              type: "address",
+              value: address,
+            },
+          ],
+        }),
+      });
 
-      const hash = deposit.txHash();
-      const depositLog = (msg: any) =>
-        console.log(
-          `BTC deposit: ${Bitcoin.utils.transactionExplorerLink(
-            deposit.depositDetails.transaction,
-            "testnet"
-          )}\n
+      // Modify below code
+      alert(`Deposit ${amount} BTC to ${mint.gatewayAddress}`);
+      mint.on("deposit", async (deposit) => {
+        // Details of the deposit are available from `deposit.depositDetails`.
+
+        const hash = deposit.txHash();
+        const depositLog = (msg: any) =>
+          console.log(
+            `BTC deposit: ${Bitcoin.utils.transactionExplorerLink(
+              deposit.depositDetails.transaction,
+              "testnet"
+            )}\n
           RenVM Hash: ${hash}\n
           Status: ${deposit.status}\n
           ${msg}`
-        );
+          );
 
-      await deposit
-        .confirmed()
-        .on("target", (target) => depositLog(`0/${target} confirmations`))
-        .on("confirmation", (confs, target) =>
-          depositLog(`${confs}/${target} confirmations`)
-        );
+        await deposit
+          .confirmed()
+          .on("target", (target) => depositLog(`0/${target} confirmations`))
+          .on("confirmation", (confs, target) =>
+            depositLog(`${confs}/${target} confirmations`)
+          );
 
-      await deposit
-        .signed()
-        // Print RenVM status - "pending", "confirming" or "done".
-        .on("status", (status) => depositLog(`Status: ${status}`));
+        await deposit
+          .signed()
+          // Print RenVM status - "pending", "confirming" or "done".
+          .on("status", (status) => depositLog(`Status: ${status}`));
 
-      await deposit
-        .mint()
-        // Print Ethereum transaction hash.
-        .on("transactionHash", (txHash) =>
-          console.log(`Ethereum transaction: ${String(txHash)}\nSubmitting...`)
-        );
+        await deposit
+          .mint()
+          // Print Ethereum transaction hash.
+          .on("transactionHash", (txHash) =>
+            console.log(
+              `Ethereum transaction: ${String(txHash)}\nSubmitting...`
+            )
+          );
 
-      console.log(`Deposited ${amount} BTC.`);
-    });
+        console.log(`Deposited ${amount} BTC.`);
+        throwConfetti();
+      });
+    } catch (error) {
+      console.log(error);
+      alert("An error occured. See console for details");
+    }
   }
   return (
     <Flex justifyContent={"center"} alignItems={"center"} marginTop={4}>
-      <Button colorScheme={"blue"} onClick={flippening}>
+      <Button colorScheme={"blue"} onClick={throwConfetti}>
         Proceed 🚀
       </Button>
     </Flex>
